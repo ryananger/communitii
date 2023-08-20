064b23fa0b923b75e6d26e793589c2cb69714914
ryananger <nightcrawling@gmail.com>
2023-08-20 12:42:34 -0400

    ...

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const https  = require('https');
+const http   = require('http');
+const fs     = require('fs');
+const cors   = require('cors');
+const path   = require('path');
+const app    = express();
+
+const dist = path.join(__dirname, '../client/dist');
 
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
@@ -9,12 +17,18 @@ app.get('*.js', function (req, res, next) {
   next();
 });
 
+// var options = {
+//   key:  fs.readFileSync(process.env.HTTPS_KEY),
+//   cert: fs.readFileSync(process.env.HTTPS_CERT)
+// };
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(dist));
 
 const PORT = 4001;
 
-app.listen(PORT);
+http.createServer(app).listen(PORT);
+// https.createServer(options, app).listen(443);
 console.log(`Server listening at http://localhost:${PORT}`);
