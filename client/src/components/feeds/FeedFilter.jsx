413b84baf5112cabbd1710358b2575d9863fdbf5
ryananger <nightcrawling@gmail.com>
2024-01-31 11:48:40 -0500

    FeedFilter functional

--- client/src/components/feeds/FeedFilter.jsx
+++ client/src/components/feeds/FeedFilter.jsx
@@ -1,15 +1,33 @@
 import React, {lazy, useEffect, useState} from 'react';
+import st from 'ryscott-st';
 
 const FeedFilter = function({feed}) {
   var colors = ['home', 'learn', 'grow', 'work', 'play', 'help'];
+
+  var toggleFilter = function(color) {
+    var filter = st.homeFilter;
+
+    if (filter.includes(color)) {
+      var split = filter.split(color + ' ');
+
+      filter = split[0].concat(split[1]);
+
+      st.setHomeFilter(filter);
+    } else {
+      st.setHomeFilter(filter + color + ' ');
+    }
+  };
 
   var renderFilters = function() {
     var rendered = [];
 
     colors.map(function(color) {
+      var style = {backgroundColor: `var(--${color})`, color: `var(--${color})`};
+      var tag = !st.homeFilter.includes(color) ? 'filterOff' : '';
+
       rendered.push(
-        <div className='filterCircle' style={{backgroundColor: `var(--${color})`, color: `var(--${color})`}}>
-          <div className='filterLine'/>
+        <div key={`${color}Filter`} className={`filterCircle ${tag}`} style={style} onClick={()=>{toggleFilter(color)}}>
+            <div className='filterLine'/>
         </div>
       );
     });
--- client/src/components/feeds/Home.jsx
+++ client/src/components/feeds/Home.jsx
@@ -5,20 +5,35 @@ import {helpers, firebase} from 'util';
 import SubmitPost from './SubmitPost.jsx';
 import Feed from './Feed.jsx';
 
+var allString = 'home learn grow work play help ';
+
 const Home = function() {
   const feeds = st.community.feeds;
+  const [feed, setFeed] = useState([]);
+  const [homeFilter, setHomeFilter] = st.newState('homeFilter', useState(st.homeFilter || allString));
 
-  const feed = [
-    ...feeds.home,
-    ...feeds.learn,
-    ...feeds.grow,
-    ...feeds.work,
-    ...feeds.play,
-    ...feeds.help
-  ];
+  var handleFilter = function() {
+    var newFeed = [];
 
-  helpers.sortFeed(feed);
+    var filterFeed = function(val) {
+      if (!val) return;
 
+      if (homeFilter.includes(val)) {
+        newFeed = [...newFeed, ...feeds[val]];
+      }
+    };
+
+    var split = homeFilter.split(' ');
+
+    for (var i = 0; i < split.length; i++) {
+      filterFeed(split[i]);
+    }
+
+    helpers.sortFeed(newFeed);
+    setFeed(newFeed);
+  };
+
+  useEffect(handleFilter, [homeFilter]);
   useEffect(()=>{}, [feed]);
 
   return (
